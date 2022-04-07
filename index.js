a27c85d0f743f2400bc72947e404bded23cd846a
buiatuan <devils2tuan@gmail.com>
2022-04-08 00:07:25 +0700

    update file ejs

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ app.use(express.static("public"));
 app.get('/', function (req, res) {
     res.render("home");
 });
+
+// About us
+app.get('/about-us', function (req, res) {
+    res.render("about_us");
+});
+
+// Contact
+app.get('/contact', function (req, res) {
+    res.render("contact");
+});
+
+// Help support
+app.get('/help-support', function (req, res) {
+    res.render("help_support");
+});
+
+// Privacy policy
+app.get('/privacy-policy', function (req, res) {
+    res.render("privacy_policy");
+});
