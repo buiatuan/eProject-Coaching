92d0537cf7c0b7a30d8e1bb390f638da4692b1c1
buiatuan <devils2tuan@gmail.com>
2022-04-27 20:42:25 +0700

    update category, pagination

--- public/js/header.js
+++ public/js/header.js
@@ -1,9 +1,9 @@
 
 const activePage = window.location.pathname;
-console.log(activePage);
+// console.log(activePage);
 
 const navLinks = document.querySelectorAll(".nav-menu .nav-menu__item").forEach(link =>{
-    console.log(link.href);
+    // console.log(link.href);
         if(link.href === `http://localhost:2909${activePage}`) {
             link.classList.add('nav-menu__item-active');
         }
